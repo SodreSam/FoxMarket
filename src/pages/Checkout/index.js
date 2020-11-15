bd2e8554a3979ed4fd963ef51cdffcbe6e5a3af5
Samuel <sodrer.samuel@gmail.com>
2020-11-15 01:32:24 -0300

    Atualização página checkout

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { FiArrowLeftCircle } from 'react-icons/fi';
+import { FiArrowLeftCircle, FiShoppingBag } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
 
 import './styles.css';
 
+import bag from '../../assets/bag.png';
 
 export default function Checkout(){
     return(
@@ -13,7 +14,16 @@ export default function Checkout(){
         <Link to="/" className="arrow-back">
             <FiArrowLeftCircle  size={35} color="#fff"/>
         </Link>
-        <h1>Carrinho de compras</h1>
+        <div className="checkout-container">
+            <h1 className="end-title">Finalizar pedido</h1>
+            <div className="checkout-content">
+                 <img className="bag" src={bag}  alt="bag"/>
+                 <h1>FoxMarket</h1>
+                 
+            </div>
+            <button className="button">Finalizar Compra</button>
+        </div>
+        
         </div>
     );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,138 +1,140 @@
-import React, {useState} from 'react';
-import Select from 'react-select'
-import { FiShoppingCart } from 'react-icons/fi';
+import React, { useState } from "react";
+import Select from "react-select";
+import { FiShoppingCart } from "react-icons/fi";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import './styles.css';
+import "./styles.css";
 
-import coca from '../../assets/coca.png';
-import fanta from '../../assets/fanta.png';
-import bolo from '../../assets/bolo.png';
-
+import coca from "../../assets/coca.png";
+import fanta from "../../assets/fanta.png";
+import bolo from "../../assets/bolo.png";
 
 const options = [
-    { id:0, value: 'Bebidas', label: 'Bebidas' },
-    { value: 'Doces', label: 'Doces' },
-    { value: 'Salgados', label: 'Salgados' }
-]
+  { id: 0, value: "Bebidas", label: "Bebidas" },
+  { id: 1, value: "Doces", label: "Doces" },
+  { id: 2, value: "Salgados", label: "Salgados" }
+];
 
 const items = [
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Fanta Uva Lata",
-        description: "Fanta Uva lata 350ml",
-        price: 3.50,
-        image: fanta
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Fanta Uva Lata",
-        description: "Fanta Uva lata 350ml",
-        price: 3.50,
-        image: coca
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Bolo de Chocolate",
-        description: "Bolo de Chocolate",
-        price: 5.00,
-        image: bolo
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Fanta Uva Lata",
-        description: "Fanta Uva lata 350ml",
-        price: 3.50,
-        image: fanta
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Fanta Uva Lata",
-        description: "Fanta Uva lata 350ml",
-        price: 3.50,
-        image: fanta
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Fanta Uva Lata",
-        description: "Fanta Uva lata 350ml",
-        price: 3.50,
-        image: fanta
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Bolo de Chocolate",
-        description: "Bolo de Chocolate",
-        price: 5.00,
-        image: bolo
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Fanta Uva Lata",
-        description: "Fanta Uva lata 350ml",
-        price: 3.50,
-        image: fanta
-    },
-    {
-        id: 0,
-        idCategory: 0,
-        name: "Bolo de Chocolate",
-        description: "Bolo de Chocolate",
-        price: 5.00,
-        image: bolo
-    },
-]
+  {
+    id: 0,
+    idCategory: 0,
+    name: "Fanta Uva Lata",
+    description: "Fanta Uva lata 350ml",
+    price: 3.5,
+    image: fanta
+  },
+  {
+    id: 1,
+    idCategory: 0,
+    name: "Fanta Uva Lata",
+    description: "Fanta Uva lata 350ml",
+    price: 3.5,
+    image: coca
+  },
+  {
+    id: 2,
+    idCategory: 1,
+    name: "Bolo de Chocolate",
+    description: "Bolo de Chocolate",
+    price: 5.0,
+    image: bolo
+  },
+  {
+    id: 3,
+    idCategory: 0,
+    name: "Fanta Uva Lata",
+    description: "Fanta Uva lata 350ml",
+    price: 3.5,
+    image: fanta
+  },
+  {
+    id: 4,
+    idCategory: 0,
+    name: "Fanta Uva Lata",
+    description: "Fanta Uva lata 350ml",
+    price: 3.5,
+    image: fanta
+  },
+  {
+    id: 5,
+    idCategory: 0,
+    name: "Fanta Uva Lata",
+    description: "Fanta Uva lata 350ml",
+    price: 3.5,
+    image: fanta
+  },
+  {
+    id: 6,
+    idCategory: 1,
+    name: "Bolo de Chocolate",
+    description: "Bolo de Chocolate",
+    price: 5.0,
+    image: bolo
+  },
+  {
+    id: 7,
+    idCategory: 2,
+    name: "Fanta Uva Lata",
+    description: "Fanta Uva lata 350ml",
+    price: 3.5,
+    image: fanta
+  },
+  {
+    id: 8,
+    idCategory: 0,
+    name: "Bolo de Chocolate",
+    description: "Bolo de Chocolate",
+    price: 5.0,
+    image: bolo
+  }
+];
 
-export default function Main(){
+export default function Main() {
+  const [carrinho, setCarrinho] = useState(0);
+  const [data, setData] = useState(items);
 
-    const [carrinho,setCarrinho] = useState(0);
+  return (
+    <>
+      <div className="main-container">
+        <Link to="/checkout" className="carrinho">
+          <FiShoppingCart size={30} color="#fff" />
+        </Link>
 
+        <Select
+          className="select"
+          placeholder="Selecione a categoria do produto"
+          onChange={({ value }) => {
+            const { id } = options.find((obj) => obj.value === value);
 
-    return(
-        <>
-        <div className="main-container">
+            setData(items.filter((obj) => obj.idCategory === id));
+          }}
+          options={options}
+        />
 
-                            <Link to="/checkout" className="carrinho">
+        <div className="content">
+          <div className="products-list">
+            {data.map((item) => (
+              <div key={item.id} className="list">
+                <img className="drinks" src={item.image} alt="drinks" />
 
-                                <FiShoppingCart size={30} color="#fff"/>
+                <p>{item.name}</p>
 
-                            </Link>
+                <h1>R${item.price}</h1>
 
-                           <Select className="select" placeholder="Selecione a categoria do produto" options={options} />
-
-            <div className="content">
-                
-                <div className="products-list">
-                
-                    { items.map(item => (
-
-                        <div className="list"> 
-
-                            <img className="drinks" src={item.image} alt="drinks"/>
-
-                            <p>{item.name}</p>
-                                    
-                            <h1>R${item.price}</h1> 
-
-                            <button onClick={() => setCarrinho(carrinho+1)} className="button">Adicionar ao carrinho</button>
-                            
-                        </div>
-                    ))}
-                   
-                </div>
-
-            </div> 
-            
-        </div><h1 className="counter">{carrinho}</h1>
-        </>
-    );
+                <button
+                  onClick={() => setCarrinho(carrinho + 1)}
+                  className="button"
+                >
+                  Adicionar ao carrinho
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <h1 className="counter">{carrinho}</h1>
+    </>
+  );
 }
